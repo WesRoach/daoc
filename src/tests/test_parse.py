9fb63148ef488a7ff48f7bb276c3800c36409028
WesRoach <wesr000@gmail.com>
2020-07-17 17:16:51 -0500

    Fix parse_item tests.

--- src/tests/test_parse.py
+++ src/tests/test_parse.py
@@ -23,5 +23,19 @@ def test_parse_itemized_log(txt=chatlog_single_item_txt):
 
 def test_parse_item(txt=chatlog_single_item_txt):
     itemized_log = itemize_log(txt)
-    parsed_itemized_log = parse_itemized_log(itemized_log)
-    assert parse_item(parsed_itemized_log["items"][0]) is not None
+    res = parse_item(itemized_log["items"][0])
+    exp = {
+        "magical_bonuses": {
+            "dexterity": "10",
+            "constitution": "9",
+            "quickness": "1",
+            "slash": "5",
+            "parry": "2",
+            "all_melee_weapon_skills": "1",
+        },
+        "item_name": "Edgebender Arcanium Exceptional Moon Claw",
+        "total_utility": "43.333",
+        "single_skill_utility": "38.333",
+    }
+
+    assert res == exp
