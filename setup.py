c042baec344e3ab55334c3b147eeae7aea1aa04f
WesRoach <wesr000@gmail.com>
2020-07-16 20:34:06 -0500

    Getting things started...

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="daoc",
+    version="0.0.1",
+    author="Wes Roach",
+    author_email="wesr000@gmail.com",
+    description="CLI Utility for DAoC",
+    packages=setuptools.find_packages(),
+    install_requires=["click"],
+    entry_points="""
+        [console_scripts]
+        daoc=daoc:cli
+    """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
